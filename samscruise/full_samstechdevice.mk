4b3c05502811ee49245baf22c485e585370bb9c5
Sams Tech Infrastructure <samstechinfra@gmail.com>
2018-04-20 11:23:33 -0500

    Added Cruise Device

---
+++ samscruise/full_samstechdevice.mk
@@ -0,0 +1,14 @@
+PRODUCT_NAME := samscruise
+PRODUCT_DEVICE := samscruise
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Sams Club Device (Cruise)
+PRODUCT_LOCALES := en_US
+
+# default is nosdcard, S/W button enabled in resource
+DEVICE_PACKAGE_OVERLAYS := device/samscruise/goi_overlays
+PRODUCT_CHARACTERISTICS := nosdcard
+
+config_toastDefaultGravity=top|center_horizontal
+
+
+OUT_DIR := out_samscuise
---
+++ samscruise/vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo full_samstechdevice-userdebug
+add_lunch_combo full_samstechdevice-user
+add_lunch_combo full_samstechdevice-eng
