c1064f0436690a6767b0ae252cffa389eee294a1
coommen <coommen@users.noreply.github.com>
2018-04-20 21:03:40 +0000

    lunch Menus works

--- samscruise/full_samstechdevice.mk
+++ samscruise/full_samstechdevice.mk
@@ -1,14 +1,14 @@
-PRODUCT_NAME := samscruise
+PRODUCT_NAME := full_samstechdevice
 PRODUCT_DEVICE := samscruise
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Sams Club Device (Cruise)
 PRODUCT_LOCALES := en_US
 
 # default is nosdcard, S/W button enabled in resource
-DEVICE_PACKAGE_OVERLAYS := device/samscruise/goi_overlays
+DEVICE_PACKAGE_OVERLAYS := device/samsclub/samscruise/goi_overlays
 PRODUCT_CHARACTERISTICS := nosdcard
 
 config_toastDefaultGravity=top|center_horizontal
 
-
+include device/samsclub/samscruise/samscruise.mk 
 OUT_DIR := out_samscuise
