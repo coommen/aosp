1cd0f4e609434ff7314cc6de118dbdc3d9e09b06
coommen <coommen@users.noreply.github.com>
2018-04-21 17:38:02 +0000

    emulator still doesn't work
    Menu works but Emulator still won't work.

--- seuic_cruise/BoardConfig.mk
+++ seuic_cruise/BoardConfig.mk
@@ -2,5 +2,21 @@
 # this board should really point to the device specific board
 # for SEUIC Cruise, provided by the device manufacturer
 
+# the x86_64 Board doesn't seem to start in the emulator
+#include $(SRC_TARGET_DIR)/board/generic_x86_64/BoardConfig.mk
 
-include $(SRC_TARGET_DIR)/board/generic_x86_64/BoardConfig.mk
+# Apparently the boards from the SRC_TARGET_DIR don't seem to work
+# SRC_TARGET_DIR = /aosp/nougat/build/target
+#include $(SRC_TARGET_DIR)/board/generic_x86/BoardConfig.mk
+
+#Use the board from "Device" folder in AOSP
+include device/generic/x86/BoardConfig.mk
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 419430400
+BOARD_CACHEIMAGE_PARTITION_SIZE := 69206016
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 512
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
